1a9b5f0870475f5ac10b93bb2c0ffbbdf80bd320
mkuin <marrit.kuin@gmail.com>
2013-09-30 09:30:45 +0200

    Update Individual.java
    crossover geschreven

--- evolution/Individual.java
+++ evolution/Individual.java
@@ -23,8 +23,20 @@ class Individual {
 	}
 
 	private double[] crossover (double[] d) { //one of the parent values
-
-		return new double[1];
+		int crossoverpoint = randomWithRange(1,9); //bepaald welk stuk van welke ouder
+		double[] child = new double[numDimensions];
+		for(int i=0, i= crossoverpoint, i++){ 			//ouder 1
+			child[i] = this.values[i];
+		}
+		for(int i= crossoverpoint + 1, i=numDimensions, i++){ //ouder 2
+			child[i]=d.values[i];
+		}
+		return child;
+	}
+	
+	private int randomWithRange(int min, int max){ 
+		 int range = (max - min) + 1;     
+		 return (int)(Math.random() * range) + min;
 	}
 
 	private double[] mutate (double[] d) {	//child values
